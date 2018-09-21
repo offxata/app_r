d43555fbdb6d7c314196ba2decfd4ccce012fe0e
offxata <offxataloss@gmail.com>
2018-09-21 17:11:59 +0300

    checking if array is empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -9,14 +9,19 @@ import classes from './Burger.scss';
 
 const Burger = (props) => {
 
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
           return <BurgerIngredient key={ igKey + i } type={ igKey }/>
         });
-    });
-    
-  console.log(transformedIngredients);
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>please, start adding ingredients</p>;
+  }
   
   return (
     <div className={classes.Burger}>
